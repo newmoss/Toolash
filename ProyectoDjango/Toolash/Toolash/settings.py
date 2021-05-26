ad73b0d0a6e5f6eb3aa7833e2ee9bec8ba44385d
newmoss <migu.badilla@alumnos.duoc.cl>
2021-05-26 16:36:25 -0400

    BASE DE DATOS
    create user py identified by 1234;

--- ProyectoDjango/Toolash/Toolash/settings.py
+++ ProyectoDjango/Toolash/Toolash/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'Toolash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'py',
+        'PASSWORD': '1234',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
     }
 }
 
